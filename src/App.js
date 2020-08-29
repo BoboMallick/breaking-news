5901e9f61b1cf99bc3df3691270073ff92e72209
bobo mallick <mallickbobo@gmail.com>
2020-08-29 12:41:28 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import News from './Components/News/News';
 import Header from './Components/Header/Header';
 import TopHeadline from './Components/TopHeadline/TopHeadline';
+import Fragment from './Components/Fragment/Fragment'
 
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Fragment></Fragment>
       <TopHeadline></TopHeadline>
     </div>
   );
